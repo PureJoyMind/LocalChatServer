0987ff53e46c2f8309b73204b80b23f304c940d7
PureJoyMind <mazdak.parniandg@gmail.com>
2024-01-15 00:07:51 +0330

    Added Session and Message Models

---
+++ LocalChatServerWeb/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using MongoDbGenericRepository.Attributes;
+
+namespace LocalChatServerWeb.Models
+{
+    [CollectionName("Messages")]
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Session Session { get; set; }
+        public ApplicationUser Sender { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ LocalChatServerWeb/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using MongoDbGenericRepository.Attributes;
+
+namespace LocalChatServerWeb.Models
+{
+    [CollectionName("Sessions")]
+    public class Session
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public ApplicationUser Creator { get; set; }
+        public bool State { get; set; }
+        public List<ApplicationUser> ActiveUsers { get; set; }
+    }
+}
